d33f6d7c9da2e8c5d051df2fb36a88e7b3bd9392
ryantsui1109 <s1106219@taivs.tp.edu.tw>
2022-09-07 15:59:04 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("%s","我愛\n中秋節"); 
+	return 0;
+}
+
